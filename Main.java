38659f4590805da224cfac1a52321cb71b0d8c11
Emily Mendoza <evmendoza37@gmail.com>
2020-02-08 17:05:34 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Coder {
+  String name;
+  int age;
+  String gender;
+  boolean liketocode;
+}
+Coder() {
+    this.name = ;
+    this.brand = "Pepsi";
+    this.color = "blue";
+    this.price = 1.75;
+    this.isEmpty = false;
+    this.size = "MED";
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
