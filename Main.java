0de6b36c3328941f48e783940eb3487071160aa2
zainabmohammedr <zainabmohammedr@gmail.com>
2020-02-15 17:42:24 +0000

    Message Change

--- Main.java
+++ Main.java
@@ -1,18 +1,28 @@
+//inintiatlizing first Coder and traits
 class Coder {
   String name;
   int age;
   String gender;
   boolean liketocode;
+  String eyecolor;
+  String hair;
+  String haircolor;
+
+//first being 
+  Coder() {
+    this.name = "Alyssa";
+    this.age = 0;
+    this.gender = "female";
+    this.liketocode = true;
+    this.eyecolor = "brown";
+    this.hair = "straight";
+    this.haircolor = "rainbow";
+  }
+
 }
-Coder() {
-    this.name = ;
-    this.brand = "Pepsi";
-    this.color = "blue";
-    this.price = 1.75;
-    this.isEmpty = false;
-    this.size = "MED";
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Hello World! My name is Alyssa and I like to code.");
   }
 }
